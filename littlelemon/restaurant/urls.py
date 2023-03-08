6320944713032c32d8c5d2631dc86b59222d83d7
martin_chakarov <martin.d.chakarov@gmail.com>
2023-03-09 00:34:03 +0200

    Create views for Menu and Booking

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import MenuItemsView, SingleMenuItemView
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view())
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+# Create your views here.
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        return super().get_queryset().filter(id=self.kwargs['pk'])
+    
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
