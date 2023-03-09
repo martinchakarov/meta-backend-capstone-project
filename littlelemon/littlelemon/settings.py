9022ef7388484c0ee09ef1493c196b847ddb342e
martin_chakarov <martin.d.chakarov@gmail.com>
2023-03-10 01:29:46 +0200

    Update static files

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'restaurant/static/'
+STATIC_URL = 'restaurant/static/restaurant/'
 
 STATICFILES_DIRS = [
-    'restaurant/static',
+    'restaurant/static/restaurant',
 ]
 
 # Default primary key field type
--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
